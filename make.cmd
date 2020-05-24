4b3df42fe88ec034028b453be42d2febfc4dd4e5
nikos chalikias <nikos.chalikias@gmail.com>
2020-05-24 15:42:31 +0100

    passed eurocircuits visualizer

---
+++ make.cmd
@@ -0,0 +1,7 @@
+    call .\template\review.cmd  
+    call .\template\gerber.cmd UPA-DIP V1 
+    call .\template\asm.cmd  
+    call .\template\r-LP.cmd  
+    call .\template\purchase.cmd 
+    call .\template\deliverables.cmd UPA-DIP V1  
+    call .\template\requirements.cmd 
