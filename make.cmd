d99adb8253d4c3ab1fa48db7d74f8a0768fc8365
nikos chalikias <nikos.chalikias@gmail.com>
2021-09-12 10:56:08 +0100

    test autoroute and upload to manufacturer

--- make.cmd
+++ make.cmd
@@ -1,7 +1,7 @@
     call .\template\review.cmd  
-    call .\template\gerber.cmd UPA-DIP V1 
+    call .\template\gerber.cmd UPA-DIP V2 
     call .\template\asm.cmd  
     call .\template\r-LP.cmd  
     call .\template\purchase.cmd 
-    call .\template\deliverables.cmd UPA-DIP V1  
+    call .\template\deliverables.cmd UPA-DIP V2  
     call .\template\requirements.cmd 
